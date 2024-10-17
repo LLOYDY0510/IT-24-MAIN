cac143066a703698e0fe3742f52c204d823f23fc
LLOYDY0510 <johnlloyddonque18@gmail.com>
2024-10-17 21:51:45 +0800

    added text input

---
+++ applet5.js
@@ -0,0 +1,8 @@
+class WeatherApp {
+  constructor(apiKey) {
+      this.apiKey = apiKey;
+      //Text Input
+      this.cityInput = document.getElementById('cityInput');
+      this.getWeatherBtn = document.getElementById('getWeatherBtn');
+
+ 
