4ad0f666cd867aafce3a57488fe24c3cf3b0698e
LLOYDY0510 <johnlloyddonque18@gmail.com>
2024-10-17 21:56:07 +0800

    event listener

--- applet5.js
+++ applet5.js
@@ -13,3 +13,7 @@ class WeatherApp {
       this.description = document.getElementById('description');
       this.humidity = document.getElementById('humidity');
       this.windSpeed = document.getElementById('windSpeed');
+     
+      this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+      this.getLocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
+  }
